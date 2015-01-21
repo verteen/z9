ff96acf601c17f9a106de3273cac1ab700c0df0e
ayurjev <andrey.yurjev@gmail>
2015-01-21 14:23:32 +0400

    Используем https для git clone

--- bin/setup_environment.sh
+++ bin/setup_environment.sh
@@ -21,25 +21,25 @@ PACKAGES="/var/z9/virtualenv/lib/python3.4/site-packages"
 cd $PACKAGES
 
 if ! [ -d $PACKAGES/envi ]; then
-    git clone git@github.com:ayurjev/envi.git
+    git clone https://github.com/ayurjev/envi.git
 else
     (echo "Envi" && cd $PACKAGES/envi && git pull)
 fi
 
 if ! [ -d $PACKAGES/mapex ]; then
-    git clone git@github.com:ayurjev/mapex.git
+    git clone https://github.com/ayurjev/mapex.git
 else
     (echo "Mapex" && cd $PACKAGES/mapex && git pull)
 fi
 
 if ! [ -d $PACKAGES/suit ]; then
-    git clone git@github.com:ayurjev/suit.git
+    git clone https://github.com/ayurjev/suit.git
 else
     (echo "Suit" && cd $PACKAGES/suit && git pull)
 fi
 
 if ! [ -d $PACKAGES/z9 ]; then
-    git clone git@github.com:ayurjev/z9.git
+    git clone https://github.com/ayurjev/z9.git
 else
     (echo "Z9" && cd $PACKAGES/z9 && git pull)
 fi
