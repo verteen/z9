fd5d9e57ac286f9935c78e8f7d42b6b7bdc237bb
Niev <evselyev@yandex.ru>
2015-04-03 13:48:34 +0300

    fix определение активности пункта меню

--- core/web/models.py
+++ core/web/models.py
@@ -40,7 +40,7 @@ class MenuItem(object):
             "alias": self.alias,
             "css_class": self.css_class,
             "childs": self.sub_menu.get_full(),
-            "active": url.startswith(self.url)
+            "active": (url.rstrip("/") + "/").startswith(self.url.rstrip("/") + "/")
         }
 
     @property
